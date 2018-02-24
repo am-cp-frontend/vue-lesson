947e7ca6f1ff5fa55210b4963d230220ee2705dc
v-trof <seva.trofimov2@gmail.com>
2018-02-24 13:49:37 +0300

    Layed foundation for the lesson

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id='app'>
+    A vue App
+  </div>
+</template>
+
+<style>
+body, html {
+  padding: 0;
+  margin: 0;  
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100vw;
+  min-height: 100vh;
+
+  font-family: 'Arial', sans-serif;
+}
+</style>
+yarn 
